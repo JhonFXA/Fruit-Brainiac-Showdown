f2488d58012ab683c7cb2a5dd2eb6632c330a097
Igor S <145506489+searaigor@users.noreply.github.com>
2023-09-20 17:11:29 -0300

    dando forma ao nosso jogo
    fizemos a divisão das telas dos jogadores;
    montamos a grade das cartas;
    e implementamos uma função recursiva para girar as cartas

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const carta= document.querySelector(".carta")
+const cartas= document.querySelectorAll(".carta")
 
-carta.addEventListener("click", ()=>{
-    carta.classList.toggle("virarCarta")
-} )
+// for(let i = 0; i<cartas.length; i++){
+//     cartas[i].addEventListener('click', ()=>{
+//         cartas[i].classList.toggle('virarCarta')
+//     })
+// }
+
+const carta = (cartas,i = 0) => {
+    if(i<cartas.length){
+        cartas[i].addEventListener('click', ()=>{
+            cartas[i].classList.toggle('virarCarta')
+        })
+        return carta(cartas, i+1)
+    }
+}
+carta(cartas)
