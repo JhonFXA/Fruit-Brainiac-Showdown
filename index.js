77b00c6894b9abaad9e1b5468d75eef68bf6e1df
Jonathan Henrique <jonathanbispo085@gmail.com>
2023-09-21 09:18:48 -0300

    Implemento de funções para criar cartas

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
+
+const criarCartas = () => {
+    const criarElemento = (tag,classe) => {
+        const elemento = document.createElement(tag)
+        elemento.className = classe
+        return elemento
+    }
+    const carta = criarElemento('div','carta')
+    const frente = criarElemento('div','frente face')
+    const verso = criarElemento('div','verso face')
+    
+    carta.appendChild(frente)
+    carta.appendChild(verso)
+    return carta;
+}
+
+const carregarGrade = () => {
+    const frutas = ['laranja','cereja','cajú','maçã','banana','côco','morango']
+    const container = document.querySelector('.container')
+    frutas.forEach(fruta => {
+        const carta = criarCartas()
+        container.appendChild(carta)
+    })
+}
+carregarGrade()
 const cartas= document.querySelectorAll(".carta")
 
-// for(let i = 0; i<cartas.length; i++){
-//     cartas[i].addEventListener('click', ()=>{
-//         cartas[i].classList.toggle('virarCarta')
-//     })
-// }
-
-const carta = (cartas,i = 0) => {
+const virarCarta = (cartas,i = 0) => {
     if(i<cartas.length){
         cartas[i].addEventListener('click', ()=>{
             cartas[i].classList.toggle('virarCarta')
         })
-        return carta(cartas, i+1)
+        return virarCarta(cartas, i+1)
     }
 }
-carta(cartas)
+virarCarta(cartas)
