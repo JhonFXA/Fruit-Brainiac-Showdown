5f05770b141c0176c29f77893934c4b9bd5490c0
JoseEduardoDuarteUfs <145577193+JoseEduardoDuarteUfs@users.noreply.github.com>
2023-09-22 01:24:39 -0300

    Adicionando imagens, randomização de cartas e mais
    Adição de comentários 
    Adição das imagens das frutas
    Randomização das cartas (incerteza sobre imutabilidade)
    Melhorando o design das cartas
    Melhorando a organização do código

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-//A função criarCartas serve para criar as cartas do jogo, como o próprio nome já diz. Dentro dela há a função criarElemento, que cria um elemento de acordo com a tag e a classe que for passada nos parâmetros. Depois de criar a carta, a frente e o verso são inseridos dentro dela e a carta é retornada.
-const criarCartas = () => {
+//A função 'criarCartas' serve para criar as cartas do jogo, como o próprio nome já diz. Dentro dela há a função 'criarElemento', que cria um elemento de acordo com a tag e a classe que for passada nos parâmetros. Depois de criar a carta, a frente e o verso são inseridos dentro dela e a carta é retornada.
+
+const frutas = ['côco','cereja','pêssego','maçã','melancia','banana','limão','morango','pepino','pera']
+
+const criarCartas = (fruta) => {
     const criarElemento = (tag,classe) => {
         const elemento = document.createElement(tag)
         elemento.className = classe
@@ -9,32 +12,39 @@ const criarCartas = () => {
     const frente = criarElemento('div','frente face')
     const verso = criarElemento('div','verso face')
     
+    
+    frente.style.backgroundImage = `url('./imagens/${fruta}.png')`
+    frente.style.backgroundPosition = 'center'
     carta.appendChild(frente)
     carta.appendChild(verso)
+
+
+
     return carta;
 }
 
+//A função 'carregarGrade' serve para criar a grade de cartas de acordo com a quantidade de frutas definidas para o jogo. Primeiramente, é criada uma lista com 10 frutas e logo abaixo há uma função recursiva para duplicar os elementos da lista, pois necessitamos dos pares. A função 'carregarGradeAux' percorre os elementos da lista recursivamente, criando uma carta para cada elemento.
 const carregarGrade = () => {
-    const frutas = ['laranja','laranja', 'cereja','cereja', 'pêssego','pêssego', 'maçã','maçã', 'melancia','melancia', 'banana','banana', 'côco', 'côco', 'morango', 'morango', 'cenoura', 'cenoura', 'pera', 'pera']
+    const duplica = [...frutas, ...frutas]
+
     const container = document.querySelector('.container')
 
     const carregarGradeAux = ([a,...b])=>{
         if(b.length==0){
             const carta = criarCartas(a)
-           return container.appendChild(carta)
+            return container.appendChild(carta)
         }else{
             const carta = criarCartas(a)
             container.appendChild(carta)
             return carregarGradeAux(b)
         }
     }
-    carregarGradeAux(frutas)
+    carregarGradeAux(duplica.sort( () => Math.random() - 0.5))
 }
 carregarGrade()
+
+//A função 'virarCarta' adiciona um evento de click para cada elemento com a classe 'carta', sempre que houver um click ela irá receber uma classe que irá rotacionar ela
 const cartas = document.querySelectorAll(".carta")
-
-
-//
 const virarCarta = (cartas,i = 0) => {
     if(i<cartas.length){
         cartas[i].addEventListener('click', ()=>{
