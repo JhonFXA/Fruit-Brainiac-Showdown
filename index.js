fd44c14142ba5b6d49adcfdfc6f0658818ebbeb9
JoseEduardoDuarteUfs <145577193+JoseEduardoDuarteUfs@users.noreply.github.com>
2023-09-29 21:20:06 -0300

    Adição das mecânicas do jogo
    - Mudanças:
    - Uso de recursividade para criar as regras de uso dos Power-ups (Muito difícil!! e muito tempo!!);
    - Melhora considerável na organização do código no HTML e Java Script (Muito difícil! e muito tempo).

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const containerUm = document.querySelector(".container1")
 const containerDois = document.querySelector(".container2")
+const pontosP1 = document.getElementById('pontosP1')
+const pontosP2 = document.getElementById('pontosP2')
+const boxHabilidades = document.querySelectorAll(".box-habilidades")
+const botoesP1 = boxHabilidades[0].querySelectorAll(".botao")
+const botoesP2 = boxHabilidades[1].querySelectorAll(".botao")
+const tempoP1 = document.getElementById('tempoP1')
+const tempoP2 = document.getElementById('tempoP2')
+
+
 const cartasViradas = []
 const frutas = [
   "côco",
@@ -24,12 +33,12 @@ const criarCartas = (fruta) => {
   const carta = criarElemento("div", "carta")
   const frente = criarElemento("div", "frente face")
   const verso = criarElemento("div", "verso face")
-
+  
   frente.style.backgroundImage = `url('./imagens/${fruta}.png')`
   frente.style.backgroundPosition = "center"
   carta.appendChild(frente)
   carta.appendChild(verso)
-
+  
   return carta
 }
 
@@ -64,37 +73,84 @@ primeiraJogada(cartas1)
 const possuiClasse = (elemento, classe) => elemento.classList.contains(classe) //Checa se um elemento possui uma classe.
 const adicionarClasse = (elemento, classe) => elemento.classList.add(classe) //Adiciona classe para o elemento.
 const removerClasse = (elemento, classe) => elemento.classList.remove(classe) //Remove classe de um elemento.
+//A função 'pausarClick' foi implementada para que os jogadores não consigam clicar em várias cartas em pouco tempo, evitando possíveis trapaças. Além disso, ela chama a si mesmo recursivamente, alterando o evento de click para cada carta.
+const pausarClick = (cartas, ordem, i = 0) => {
+  if (i < cartas.length) {
+    cartas[i].style.pointerEvents = ordem
+    return pausarClick(cartas, ordem, i + 1)
+  }
+}
+
+const existeAtivado = ([a,...b]) => {
+  if(a === undefined) return false
+  else if(a.classList.contains('ativado')) return true
+  return existeAtivado(b)
+}
+
+const removerClassePraCada = ([a, ...b], classe) => {
+  if (a) {
+    a.classList.remove(classe);
+    removerClassePraCada(b, classe);
+  }
+}
+
+const verificarPontosP1 = () =>{
+  if(!existeAtivado(botoesP1)){
+    if(pontosP1.innerHTML > 1 && !possuiClasse(botoesP1[0],"ativado")){
+      adicionarClasse(botoesP1[0],"botao-piscante-P1")
+    }
+    if(pontosP1.innerHTML > 2 && !possuiClasse(botoesP1[1],"ativado")){
+      adicionarClasse(botoesP1[1],"botao-piscante-P1")
+    }
+    if(pontosP1.innerHTML > 3 && !possuiClasse(botoesP1[2],"ativado")){
+      adicionarClasse(botoesP1[2],"botao-piscante-P1")
+    }
+    if(pontosP1.innerHTML > 4 && !possuiClasse(botoesP1[3],"ativado")){
+      adicionarClasse(botoesP1[3],"botao-piscante-P1")
+    }
+  }
+}
+
+const verificarPontosP2 = () =>{
+  if(!existeAtivado(botoesP2)){
+    if(pontosP2.innerHTML > 1 && !possuiClasse(botoesP2[0],"ativado")){
+      adicionarClasse(botoesP2[0],"botao-piscante-P2")
+    }
+    if(pontosP2.innerHTML > 2 && !possuiClasse(botoesP2[1],"ativado")){
+      adicionarClasse(botoesP2[1],"botao-piscante-P2")
+    }
+    if(pontosP2.innerHTML > 3 && !possuiClasse(botoesP2[2],"ativado")){
+      adicionarClasse(botoesP2[2],"botao-piscante-P2")
+    }
+    if(pontosP2.innerHTML > 4 && !possuiClasse(botoesP2[3],"ativado")){
+      adicionarClasse(botoesP2[3],"botao-piscante-P2")
+    }
+  }
+}
+
+
 
 const contagemP1 = (numero) => {
   const contador = document.getElementById("tempoP1")
   if (numero === 0) {
-    if (possuiClasse(botaoRepetirRodadaP1, "ativado")) {
-      adicionarClasse(containerUm, "elemento-piscante")
-      containerDois.style.opacity = 0.4
-      containerUm.style.opacity = 1
-      setTimeout(() => {
-        removerClasse(containerUm, "elemento-piscante")
-        removerClasse(botaoRepetirRodadaP1, "ativado")
-        permitirVirada(cartas1)
-        contagemP1(10)
-      }, 2000)
-    } else {
-      adicionarClasse(containerDois, "elemento-piscante")
-      containerUm.style.opacity = 0.4
-      containerDois.style.opacity = 1
-      setTimeout(() => {
-        removerClasse(containerDois, "elemento-piscante")
-        permitirVirada(cartas2)
-        contagemP2(10)
-      }, 2000)
-    }
+    adicionarClasse(containerDois, "elemento-piscante")
+    containerUm.style.opacity = 0.4
+    containerDois.style.opacity = 1
+    setTimeout(() => {
+      removerClasse(containerDois, "elemento-piscante")
+      permitirVirada(cartas2)
+      contagemP2(10)
+    }, 2000)
     negarVirada(cartas1)
     contador.innerHTML = "0"
     if (cartasViradas.length == 1) {
       removerClasse(cartasViradas[0], "virar-carta")
       cartasViradas.length = 0
     }
+    removerClassePraCada(botoesP1,'ativado')
+    removerClassePraCada(botoesP1,'botao-piscante-P1')
   } else {
+    verificarPontosP1()
     contador.innerHTML = numero
     setTimeout(() => {
       contagemP1(numero - 1)
@@ -105,18 +161,8 @@ const contagemP1 = (numero) => {
 const contagemP2 = (numero) => {
   const contador = document.getElementById("tempoP2")
   if (numero === 0) {
-    if (possuiClasse(botaoRepetirRodadaP2, "ativado")) {
-      adicionarClasse(containerDois, "elemento-piscante")
-      containerDois.style.opacity = 1
-      containerUm.style.opacity = 0.4
-      setTimeout(() => {
-        contagemP2(10)
-        permitirVirada(cartas2)
-        removerClasse(containerDois, "elemento-piscante")
-        removerClasse(botaoRepetirRodadaP2, "ativado")
-      }, 2000)
-    } else {
-      adicionarClasse(containerUm, "elemento-piscante")
+
+    adicionarClasse(containerUm, "elemento-piscante")
       containerDois.style.opacity = 0.4
       containerUm.style.opacity = 1
       setTimeout(() => {
@@ -124,14 +170,16 @@ const contagemP2 = (numero) => {
         permitirVirada(cartas1)
         contagemP1(10)
       }, 2000)
-    }
-    negarVirada(cartas2)
-    contador.innerHTML = "0"
-    if (cartasViradas.length == 1) {
-      removerClasse(cartasViradas[0], "virar-carta")
-      cartasViradas.length = 0
-    }
-  } else {
+      negarVirada(cartas2)
+      contador.innerHTML = "0"
+      if (cartasViradas.length == 1) {
+        removerClasse(cartasViradas[0], "virar-carta")
+        cartasViradas.length = 0
+      }
+      removerClassePraCada(botoesP2,'ativado')
+      removerClassePraCada(botoesP2,'botao-piscante-P2')
+    } else {
+    verificarPontosP2()
     contador.innerHTML = numero
     setTimeout(() => {
       contagemP2(numero - 1)
@@ -143,6 +191,7 @@ contagemP1(10)
 
 //A função 'virarCartaAux' funciona como um 'forEach', que para cada carta, adiciona um evento de click que chama a função 'virarCarta'.
 const virarCartaAux = (cartas, i = 0) => {
+  
   if (i < cartas.length) {
     cartas[i].addEventListener("click", () => {
       virarCarta(cartas[i])
@@ -152,68 +201,74 @@ const virarCartaAux = (cartas, i = 0) => {
 }
 virarCartaAux(cartas)
 //A função 'virarCarta' é a que faz o jogo funcionar em cada grade de cartas, ela possui uma série de outras funções e condicionais que serão explicadas a seguir.
+
+
+
 const virarCarta = (carta) => {
   if (carta.classList.contains("pode-virar")) {
     const puxarElemento = (elemPai, identificador) =>
-      elemPai.querySelector(identificador) //Puxa algum elemento para ser utilizado.
+    elemPai.querySelector(identificador) //Puxa algum elemento para ser utilizado.
     const puxarUrl = (elemento) =>
-      getComputedStyle(elemento).getPropertyValue("background-image") //Guarda a url da imagem de um elemento
-
-    //A função 'pausarClick' foi implementada para que os jogadores não consigam clicar em várias cartas em pouco tempo, evitando possíveis trapaças. Além disso, ela chama a si mesmo recursivamente, alterando o evento de click para cada carta.
-    const pausarClick = (cartas, ordem, i = 0) => {
-      if (i < cartas.length) {
-        cartas[i].style.pointerEvents = ordem
-        return pausarClick(cartas, ordem, i + 1)
-      }
-    }
-
+    getComputedStyle(elemento).getPropertyValue("background-image") //Guarda a url da imagem de um elemento
+    
+    
     //Se a carta que entrar na função como parâmetro já tiver a classe 'cartas-iguais', significa que ela já teve seu par resolvido, portanto, ela não pode ser virada novamente.
     if (possuiClasse(carta, "cartas-iguais")) return
     //Se a carta ainda não foi virada, ou seja, não possui a classe 'virar-carta' e a quantidade de cartas viradas for menor que dois, ela receberá a classe e será adicionada na lista de cartas viradas.
     else if (!possuiClasse(carta, "virar-carta") && cartasViradas.length < 2) {
       adicionarClasse(carta, "virar-carta")
       cartasViradas.push(carta)
-
+      
       //Caso já existam duas cartas viradas, temos que checar se as cartas escolhidas são iguais ou não.
       if (cartasViradas.length === 2) {
         const escolhas = [cartasViradas[0], cartasViradas[1]] //Aqui vão ficar as cartas que o jogador escolheu.
-
+        
         //Aqui vão ficar as frentes das cartas que o jogador escolheu.
         const frentes = [
           puxarElemento(escolhas[0], ".frente"),
           puxarElemento(escolhas[1], ".frente"),
         ]
-
+        
         //Aqui vão ficar os versos das cartas que o jogador escolheu.
         const versos = [
           puxarElemento(escolhas[0], ".verso"),
           puxarElemento(escolhas[1], ".verso"),
         ]
-
+        
         const urlFrutas = [puxarUrl(frentes[0]), puxarUrl(frentes[1])] //Essa lista vai guardar a url das imagens de cada frente.
         cartasViradas.length = 0 //Após a escolha de duas cartas, o número de viradas tem que voltar à zero.
         pausarClick(cartas, "none") //O clicks devem ser pausados na escolha de duas cartas.
-
         
-      // O 'setTimeout' define um atraso, tudo o que estiver dentro de seu escopo só será executado após 0.7 segundos. Está sendo utilizado para que dê tempo de o jogador ver as cartas que escolheu antes de serem viradas novamente.
+        
+        // O 'setTimeout' define um atraso, tudo o que estiver dentro de seu escopo só será executado após 0.7 segundos. Está sendo utilizado para que dê tempo de o jogador ver as cartas que escolheu antes de serem viradas novamente.
         setTimeout(() => {
           //Ambas as cartas irão perder a classe de virada e serão escondidas novamente,...
           removerClasse(escolhas[0], "virar-carta")
           removerClasse(escolhas[1], "virar-carta")
-
-           //mas caso as frutas sejam iguais, elas receberão a classe de 'cartas-iguais', que as deixará transparentes e travadas.
+          
+          //mas caso as frutas sejam iguais, elas receberão a classe de 'cartas-iguais', que as deixará transparentes e travadas.
+          
           if (urlFrutas[0] === urlFrutas[1]) {
+            escolhas[0].classList.remove("pode-virar")
+            escolhas[1].classList.remove("pode-virar")
             adicionarClasse(escolhas[0], "cartas-iguais")
             adicionarClasse(escolhas[1], "cartas-iguais")
             versos[0].style.display = "none"
             versos[1].style.display = "none"
-            const pontosP1 = puxarElemento(document, "#pontosP1")
-            const pontosP2 = puxarElemento(document, "#pontosP2")
+            
+            
+            const igualdadesP2 = containerDois.querySelectorAll(".cartas-iguais")
             const igualdadesP1 = containerUm.querySelectorAll(".cartas-iguais")
-            const igualdadesP2 =
-              containerDois.querySelectorAll(".cartas-iguais")
-            pontosP1.innerHTML = igualdadesP1.length / 2
-            pontosP2.innerHTML = igualdadesP2.length / 2
+            
+            const contadorP1 = document.getElementById('tempoP1')
+            const contadorP2 = document.getElementById('tempoP2')
+            
+            if(contadorP1.innerHTML==0){
+              pontosP2.innerHTML ++
+            }
+            if(contadorP2.innerHTML==0){
+              pontosP1.innerHTML ++
+            }
             if (igualdadesP1.length == 20) {
               window.alert("Jogador 1 venceu")
               window.location.reload()
@@ -223,7 +278,7 @@ const virarCarta = (carta) => {
               window.location.reload()
             }
           }
-
+          
           pausarClick(cartas, "all") //Retorna o evento de click nas cartas.
         }, 700)
       }
@@ -245,12 +300,66 @@ const negarVirada = (cartas, i = 0) => {
   }
 }
 
-const botaoRepetirRodadaP1 = document.getElementById("up-rodadaP1")
 const repetirRodadaP1 = () => {
-  botaoRepetirRodadaP1.classList.toggle("ativado")
+  if(pontosP1.innerHTML>1){
+  pontosP1.innerHTML -= 2
+  removerClassePraCada(botoesP1,'botao-piscante-P1')
+  adicionarClasse(botoesP1[0],"ativado")
+ }
 }
 
-const botaoRepetirRodadaP2 = document.getElementById("up-rodadaP2")
-const repetirRodadaP2 = () => {
-  botaoRepetirRodadaP2.classList.toggle("ativado")
+const botao2 = () => {
+  if(pontosP1.innerHTML>2){
+  pontosP1.innerHTML -= 3
+  removerClassePraCada(botoesP1,'botao-piscante-P1')
+  adicionarClasse(botoesP1[1],"ativado")
+   }  
 }
+
+const botao3 = () => {
+  if(pontosP1.innerHTML>3){
+  pontosP1.innerHTML -= 4
+  removerClassePraCada(botoesP1,'botao-piscante-P1')
+  adicionarClasse(botoesP1[2],"ativado")
+  }
+}
+const botao4 = () => {
+  if(pontosP1.innerHTML>4){
+    pontosP1.innerHTML -= 5
+    removerClassePraCada(botoesP1,'botao-piscante-P1')
+    adicionarClasse(botoesP1[3],"ativado")
+    }
+}
+
+const repetirRodadaP2 = () => {
+  if(pontosP2.innerHTML>1){
+  pontosP2.innerHTML -= 2
+  removerClassePraCada(botoesP2,'botao-piscante-P2')
+  adicionarClasse(botoesP2[0],"ativado")
+ }
+}
+
+const botao2P2 = () => {
+  if(pontosP2.innerHTML>2){
+    pontosP2.innerHTML -= 3
+    removerClassePraCada(botoesP2,'botao-piscante-P2')
+    adicionarClasse(botoesP2[1],"ativado")
+    }
+}
+
+const botao3P2 = () => {
+  if(pontosP2.innerHTML>3){
+    pontosP2.innerHTML -= 4
+    removerClassePraCada(botoesP1,'botao-piscante-P2')
+    adicionarClasse(botoesP2[2],"ativado")
+    }
+}
+
+const botao4P2 = () => {
+  if(pontosP2.innerHTML>4){
+    pontosP2.innerHTML -= 5
+    removerClassePraCada(botoesP2,'botao-piscante-P2')
+    adicionarClasse(botoesP2[3],"ativado")
+    }
+}
+
