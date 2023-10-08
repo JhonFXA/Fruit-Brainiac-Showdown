5d438135545c756343e98c33f0823b7d7cb29222
JoseEduardoDuarteUfs <145577193+JoseEduardoDuarteUfs@users.noreply.github.com>
2023-10-08 11:40:13 -0300

    Adições essenciais ao jogo
    Adições: 
    - Menu para inicial com opções de jogar, tutorial e créditos;
    - Ao clicar em jogar no menu inicial, os jogadores serão levados para escolher seus nomes, que aparecerão na partida e na tela de vitória;
    - Comentários ao código.
    
    O que falta:
    
    - Tutorial e créditos serão atualizados em breve.

---
+++ js/menu.js
@@ -0,0 +1,49 @@
+const usernameBox = document.querySelector('.usernames')
+const inputP1 = document.getElementById('username-P1')
+const inputP2 = document.getElementById('username-P2')
+const partida = document.querySelector('#botao-partida')
+
+
+//Função para voltar quando se clica nos botões JOGAR, TUTORIAL OU CRÉDITOS.
+const botaoVoltar = () =>{
+    partida.classList.remove('botao-partida-liberado')
+    partida.classList.add('botao-partida')
+    usernameBox.classList.add('invisibilidade')
+    inputP1.value = ''
+    inputP2.value = ''
+}
+//Função "botao jogar" serve levar a uma tela onde os jogadores poderão escolher suas siglas e, assim, poder jogar.
+const botaoJogar = () => {
+    usernameBox.classList.remove('invisibilidade')
+    inputP1.value = ''
+    inputP2.value = ''
+}
+
+
+
+//Verifica quanto caracteres foram escritos pelos usuários com a finalidade de habilitar o botão para iniciar a partida.
+
+const verificarTamanhoUsername = () =>{
+    if(inputP1.value.length>0 && inputP2.value.length>0){
+        partida.classList.remove('botao-partida')
+        partida.classList.add('botao-partida-liberado')   
+    }else {
+        partida.classList.remove('botao-partida-liberado')
+        partida.classList.add('botao-partida')
+    }
+}
+//Adiciona um evento de clique para o botão de partida.
+partida.addEventListener('click',()=>{
+    //Caso alguns dos jogadores coloquem 'espaço' no imput, o jogo mostra que não é permitido o uso desse caratér.
+    if(inputP1.value.includes(' ') || inputP2.value.includes(' ')){
+        window.alert('NÃO É PERMITIDO O USO DE ESPAÇO')
+    }
+    //Caso os jogadores tiverem os imputs validados, o botão 'partida' irá redirecioná-los para o jogo.  
+    else {
+        const usernameP1 = inputP1.value
+        const usernameP2 = inputP2.value
+        localStorage.setItem('usernameP1', usernameP1)
+        localStorage.setItem('usernameP2', usernameP2)
+        window.location.href = 'partida.html'
+    }
+})
